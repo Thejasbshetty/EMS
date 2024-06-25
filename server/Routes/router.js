5f8eee996a416f22b4146acc5524707ef5d07cf0
thejasbshetty <thejasbshetty@gmail.com>
2024-06-25 20:08:43 +0530

    chore: Add create route for user creation

---
+++ server/Routes/router.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = new express.Router();
+const connection = require('../db/connection');
+
+
+router.post("/create",(req,res)=>{
+    // console.log(req.body);
+
+    const {name,email,age,mobile,work,address,description}=req.body;
+
+    if(!name || !email || !age || !mobile || !work || !address || !description){
+        res.status(422).json({error:"Please fill the form properly"});
+    }
+
+    try {
+        connection.query("SELECT * FROM users WHERE email=?",[email],async (error,result)=>{
+            if(result.length>0){
+                res.status(422).json({error:"User already exists"});
+            }
+            else{
+                connection.query("INSERT INTO users SET ? ",{name,email,age,mobile,work,address,description},(error,result)=>{
+                    if(error){
+                        console.log("error"+error);
+                    }
+                    else{
+                        res.status(201).json(req.body);
+                    }
+                });
+            }
+        })
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+module.exports = router;
--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const mysql = require("mysql2");
 const cors = require('cors');
 require("./db/connection");
 const port = 8080;
+const router = require('./Routes/router');
 
 
 // app.get('/', (req, res) => {
@@ -13,6 +14,7 @@ const port = 8080;
 //middleware
 app.use(express.json());
 app.use(cors());
+app.use(router);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
